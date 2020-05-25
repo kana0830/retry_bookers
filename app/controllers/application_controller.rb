f40b3f22275abc79cc478a032e69adc8341a3eed
vagrant <vagrant@localhost.localdomain>
2020-05-25 13:24:33 +0000

    add: home controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   
+  # devise機能が使われる前にconfigure_permitted_parametersが実行
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # nameのデータ操作を許可
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
